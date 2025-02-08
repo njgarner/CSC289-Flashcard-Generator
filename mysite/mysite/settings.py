de041f8cc47d49c0481acd585cfef39f46b49573
njgarner <118210681+njgarner@users.noreply.github.com>
2025-02-08 11:54:38 -0500

    Update settings.py
    Adding relative database format/information from the previous settings.py file.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -70,13 +70,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
-# Database
+# Database: Information here should refer to the MySQL database. Current information is placeholder.
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'database_name',
+        'USER': 'mysql_username',
+        'PASSWORD': 'mysql_password',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
