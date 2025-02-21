52feecdd5a4a42beeb037fc74da5f55bc3236276
njgarner <118210681+njgarner@users.noreply.github.com>
2025-02-20 21:35:03 -0500

    Adding local folder
    Folder containing the files necessary for accessing the local server rather than the AWS one.

---
+++ local/__init__local.py
@@ -0,0 +1,87 @@
+import pymysql
+
+from pymysql.constants import CLIENT
+
+pymysql.install_as_MySQLdb()
+
+class Database:
+
+    ########## local database credentials ###########
+    database_cred = {
+
+        "host": "localhost",
+
+        "user": "root",
+
+        "password": "waketech",
+
+        "database": "flashcard_db",
+
+        "autocommit": True,
+
+        "cursorclass": pymysql.cursors.DictCursor,
+
+    }
+
+    def __init__(self, database_cred: dict = None):
+
+        if database_cred:
+
+            self.conn = pymysql.connect(**database_cred)
+
+        else:
+
+            self.conn = pymysql.connect(**self.database_cred)
+
+        # end if
+
+
+    def run_qry(self, sql: str):
+
+        # to check DB Connection pin the instance
+
+        self.conn.ping()
+
+        with self.conn.cursor() as cursor:
+
+            cursor.execute(sql)
+
+            self.conn.commit()
+
+            result = cursor.fetchall()
+
+        # end with
+
+        return result
+
+
+    def run_multiple_queries(self, sql_statements: list[str]):
+
+        if len(sql_statements) == 1:
+
+            result = self.run_qry(sql_statements[0])
+
+            return result
+
+
+        new_db_cred = {"client_flag": CLIENT.MULTI_STATEMENTS, **self.database_cred}
+
+        sql_statements = ";".join(sql_statements)
+
+
+        sql_connetion = pymysql.connect(**new_db_cred)
+
+        # to check DB Connection pin the instance
+
+        sql_connetion.ping()
+
+        with sql_connetion.cursor() as cursor:
+
+            affected_rows = cursor.execute(sql_statements)
+
+            self.conn.commit()
+
+            result = cursor.fetchall()
+
+        return result
+# end class
