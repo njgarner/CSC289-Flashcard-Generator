f45664cf209c18476fed0e1e92af7c5320d1df6e
Arley Zamora <AGZamora@my.waketech.edu>
2025-03-17 20:14:29 -0400

    tried to add a hint field
    Tried to add hint field to Flashcards. Publishing this branch to get help because it seems like my migrate command is messed up.

--- mysite/mysite/models.py
+++ mysite/mysite/models.py
@@ -20,6 +20,7 @@ class Flashcard(models.Model):
     answer = models.TextField(null=False)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    hint = models.TextField(blank=True)
 
 class Category(models.Model):
     category_id = models.AutoField(primary_key=True)
