75f4c2969644438837fc4408e07bb4a7104ff486
njgarner <118210681+njgarner@users.noreply.github.com>
2025-04-03 23:57:22 -0400

    Update views.py
    Invalid credentials

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -367,7 +367,7 @@ def user_login(request):  # Logs in user
             messages.success(request, "Login successful!")
             return redirect('home')
         else:
-            messages.error(request, "Invalid credentials.")
+            messages.error(request, "Invalid credentials")
             return render(request, 'login.html', {"username": username})  # Preserve input
 
     return render(request, 'login.html')
