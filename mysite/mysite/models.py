24d0710461d23fb7666465c92e5d2f762cdb9f0f
jkapp3 <139725363+jkapp3@users.noreply.github.com>
2025-04-17 21:28:09 -0400

    Update models.py

--- mysite/mysite/models.py
+++ mysite/mysite/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User  # Import the built-in User model
 from datetime import timedelta, date, datetime
 
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+
 class FlashcardSet(models.Model):
     set_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100, null=False)
