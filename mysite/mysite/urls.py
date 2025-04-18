3f74e206445df07e68629b55b7e7a401687b380b
jkapp3 <139725363+jkapp3@users.noreply.github.com>
2025-04-17 21:33:43 -0400

    Update urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,6 +20,12 @@ from django.urls import path, include
 from . import views
 from django.contrib.auth.views import LogoutView
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
